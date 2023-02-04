fd6fb573cfb2f1bb696373f521e85e2e6947a8cf
OlegPiskun <112425702+OlegPiskun@users.noreply.github.com>
2023-02-04 15:59:42 +0200

    5 tasks are ready

--- js/task-01.js
+++ js/task-01.js
@@ -15,10 +15,10 @@
 // Category: Technologies
 // Elements: 5
 
-// const categories = document.querySelectorAll(".item");
-// console.log(`Number of categories: ${categories.length}`);
+const categories = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categories.length}`);
 
-// categories.forEach((item) => {
-//   console.log(`Category: ${item.firstElementChild.textContent}`);
-//   console.log(`Elements: ${item.lastElementChild.children.length}`);
-// });
+categories.forEach((item) => {
+  console.log(`Category: ${item.firstElementChild.textContent}`);
+  console.log(`Elements: ${item.lastElementChild.children.length}`);
+});
--- js/task-03.js
+++ js/task-03.js
@@ -1,14 +1,37 @@
+// Використовуй масив об'єктів images для створення елементів <img>, вкладених в <li>. Для створення розмітки використовуй шаблонні рядки і метод insertAdjacentHTML().
+
+// Усі елементи галереї повинні додаватися в DOM за одну операцію додавання.
+// Додай мінімальне оформлення галереї флексбоксами або грідами через CSS класи.
+
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
 ];
+
+const galleryList = document.querySelector(".gallery");
+
+const markup = images
+  .map(
+    (image) =>
+      `<li><img class="photo" src="${image.url}" width="350" alt="${image.alt}"></li>`
+  )
+  .join("");
+
+galleryList.insertAdjacentHTML("beforeend", markup);
+
+const photoCards = document.querySelector(".photo");
+galleryList.style.display = "flex";
+galleryList.style.listStyleType = "none";
+galleryList.style.gap = "20px";
+galleryList.style.justifyContent = "centre";
+galleryList.style.alignItems = "center";
