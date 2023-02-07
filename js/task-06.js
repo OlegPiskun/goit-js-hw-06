10c0dd0c78d40e994a971e347d268fa1a66fde0b
OlegPiskun <112425702+OlegPiskun@users.noreply.github.com>
2023-02-07 20:46:40 +0200

    fixed task 6

--- js/task-06.js
+++ js/task-06.js
@@ -12,13 +12,12 @@
 // Для додавання стилів використовуй CSS-класи valid і invalid, які ми вже додали у вихідні файли завдання.
 
 const textInput = document.querySelector("#validation-input");
-// console.log(textInput.getAttribute("data-length"));
 
 textInput.addEventListener("blur", (event) => {
   if (event.target.value.length == textInput.getAttribute("data-length")) {
     textInput.classList.add("valid");
     if (textInput.classList.contains("invalid")) {
-      validationInput.classList.remove("invalid");
+      textInput.classList.remove("invalid");
     }
   } else {
     if (textInput.classList.contains("valid")) {
